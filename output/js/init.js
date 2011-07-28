7235b8895da39df19651d6891052a8d8a24558ec
Dientuki <dienuki@gmail.com>
2011-07-28 02:23:52 -0300

    scroll for ff, not chorme

--- output/js/init.js
+++ output/js/init.js
@@ -8,6 +8,15 @@ function fixedSidebar (sidebar){
 		$(window).unbind('scroll').bind('scroll', function(e){
 			var parent_offset = ($parent.offset().top + $parent.height()) - (height + 10);
 			var offset = window.pageYOffset;
+			var main_content = window.innerHeight/2;
+
+			$('a.anchor-item').each(function(index){
+				var t = $(this).offset().top - offset;
+				if ( t <= main_content) {
+					$bar.find('li').removeClass('selected');
+					$bar.find('li:eq(' + index + ')').addClass('selected');
+				}
+			});
 			
 			if (offset >= parent_offset) {
 				$bar.removeClass('fixed').addClass('static');
@@ -42,6 +51,7 @@ $(document).ready(function() {
 	switch ($('body').attr('id'))	{
 		case 'company':
 			fixedSidebar('#left-sidebar ul');
+			$('#left-sidebar ul').localScroll();
 		break;
 	}
 	
