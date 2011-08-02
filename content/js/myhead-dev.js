db9c73b16ce08bc95f3046e10bb18ed80ca7aff4
Juan Farias <jfarias@cmd.com.ar>
2011-08-02 15:00:30 -0300

    added images

--- content/js/myhead-dev.js
+++ content/js/myhead-dev.js
@@ -3,11 +3,6 @@ head.js('js/libs/jquery-1.6.2-min.js',
 		'js/plugins/jquery-scrollTo-1.4.2-min.js',
 		'js/plugins/jquery-localscroll-1.2.7-min.js',		
 		'js/init.js');
-/*
-'js/plugins/easySlider1.7.js',
-'js/plugins/jquery-scrollTo-1.4.2-min.js',
-'js/plugins/jquery-localscroll-1.2.7-min.js',
-*/
 
 if (head.browser.ie)  {
 	if (parseInt(head.browser.version) <= 6)   {
