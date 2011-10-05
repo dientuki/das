f57f297138d6f794bee60732e37a8d243c322fef
Dientuki <dientuki@gmail.com>
2011-10-05 12:19:52 -0300

    fix

--- output/js/png-fix.js
+++
@@ -1,5 +0,0 @@
-DD_belatedPNG.fix('.brochure a, .follow-us a, .mail-to a, .home a, .clients img');
-//DD_belatedPNG.fix('.title h1, #right-sidebar, #left-sidebar .bg-bottom, #left-sidebar .bg-top');
-//DD_belatedPNG.fix('#right-sidebar .title, .main-content .unsorted-list-title, .main-content .bg');
-DD_belatedPNG.fix('.gallery-link figure');//, .main-content .bg');
-
