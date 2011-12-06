2aa0f7eb254c125ae43ea9fdd4aa6d950ae89b9f
dientuki <dientuki@gmail.com>
2011-12-05 22:10:14 -0300

    clientes

--- content/compress.sh
+++ content/compress.sh
@@ -1,9 +1,14 @@
+echo 'starting...'
+
 mkdir -p tmp/css
 
-java -jar /usr/share/yui/yuicompressor-2.4.6.jar --type css --line-break 200 -o tmp/css/00_reset.css  css/reset.css
-java -jar /usr/share/yui/yuicompressor-2.4.6.jar --type css --line-break 200 -o tmp/css/10_style.css  css/style.css
-java -jar /usr/share/yui/yuicompressor-2.4.6.jar --type css --line-break 200 -o tmp/css/20_pikachoose.css  css/pikachoose.css
-java -jar /usr/share/yui/yuicompressor-2.4.6.jar --type css --line-break 200 -o css/ie-min.css  css/ie.css
+echo 'compresing css...'
+
+java -jar /usr/share/yui/yuicompressor-2.4.7.jar --type css --line-break 200 -o tmp/css/00_reset.css  css/reset.css
+java -jar /usr/share/yui/yuicompressor-2.4.7.jar --type css --line-break 200 -o tmp/css/10_style.css  css/style.css
+java -jar /usr/share/yui/yuicompressor-2.4.7.jar --type css --line-break 200 -o tmp/css/15_clients.css  css/clients.css
+java -jar /usr/share/yui/yuicompressor-2.4.7.jar --type css --line-break 200 -o tmp/css/20_pikachoose.css  css/pikachoose.css
+java -jar /usr/share/yui/yuicompressor-2.4.7.jar --type css --line-break 200 -o css/ie-min.css  css/ie.css
 
 cat tmp/css/* > css/das-site.css
 
@@ -16,18 +21,20 @@ mkdir -p tmp/js
 mkdir -p tmp/js/prod
 mkdir -p tmp/js/ie
 
-java -jar /usr/share/yui/yuicompressor-2.4.6.jar --type js --line-break 200 -o tmp/js/prod/10_easySlider.js js/plugins/easySlider1-7-custom.js
-java -jar /usr/share/yui/yuicompressor-2.4.6.jar --type js --line-break 200 -o tmp/js/prod/20_scrollTo.js js/plugins/jquery-scrollTo-1.4.2-min.js
-java -jar /usr/share/yui/yuicompressor-2.4.6.jar --type js --line-break 200 -o tmp/js/prod/30_localscroll.js js/plugins/jquery-localscroll-1.2.7-min.js
-java -jar /usr/share/yui/yuicompressor-2.4.6.jar --type js --line-break 200 -o tmp/js/prod/40_pikachoose.js js/plugins/jquery-pikachoose-full.js
-java -jar /usr/share/yui/yuicompressor-2.4.6.jar --type js --line-break 200 -o tmp/js/prod/50_init.js js/init.js
+echo 'compresing js...'
+java -jar /usr/share/yui/yuicompressor-2.4.7.jar --type js --line-break 200 -o tmp/js/prod/10_easySlider.js js/plugins/easySlider1-7-custom.js
+java -jar /usr/share/yui/yuicompressor-2.4.7.jar --type js --line-break 200 -o tmp/js/prod/20_scrollTo.js js/plugins/jquery-scrollTo-1.4.2-min.js
+java -jar /usr/share/yui/yuicompressor-2.4.7.jar --type js --line-break 200 -o tmp/js/prod/30_localscroll.js js/plugins/jquery-localscroll-1.2.7-min.js
+java -jar /usr/share/yui/yuicompressor-2.4.7.jar --type js --line-break 200 -o tmp/js/prod/40_pikachoose.js js/plugins/jquery-pikachoose-full.js
+java -jar /usr/share/yui/yuicompressor-2.4.7.jar --type js --line-break 200 -o tmp/js/prod/50_init.js js/init.js
 
 cat tmp/js/prod/* > js/das-js.js
 
 cp js/libs/dd_belatedpng.js tmp/js/ie/10_dd_belatedpng.js
-java -jar /usr/share/yui/yuicompressor-2.4.6.jar --type js --line-break 200 -o tmp/js/ie/20_png-fix.js js/png-fix.js
+java -jar /usr/share/yui/yuicompressor-2.4.7.jar --type js --line-break 200 -o tmp/js/ie/20_png-fix.js js/png-fix.js
 
 cat tmp/js/ie/* > js/png-fix-min.js
 
 rm -rf tmp
 
+echo 'Ta dan!'
--- output/js/myhead-prod.js
+++
@@ -1,2 +0,0 @@
-head.js('js/libs/jquery-1.6.2-min.js','js/das-js.js');
-if (head.browser.ie) {if (parseInt(head.browser.version) <= 6) {head.js('js/png-fix-min.js')}}
